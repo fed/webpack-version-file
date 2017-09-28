1f346c4e9c19f29829335d3f0f58801b3e6c0936
Federico Knüssel <fknussel@gmail.com>
2017-09-27 21:13:13 -0300

    Rewrite tests using Chai

--- test/misc.js
+++ test/misc.js
@@ -1,20 +1,20 @@
-import expect from 'expect';
+import {expect} from 'chai';
 import VersionFile from '../lib/index';
 
 describe('Version File Webpack Plugin', () => {
   it('can be instantiated', () => {
     const plugin = new VersionFile();
 
-    expect(plugin).toExist();
+    expect(plugin).to.exist;
   });
 
   it('is a function constructor', () => {
-    expect(VersionFile).toBeA(Function);
+    expect(VersionFile).to.be.a('function');
   });
 
   it('has an apply method', () => {
     const plugin = new VersionFile();
 
-    expect(plugin.apply).toExist();
+    expect(plugin.apply).to.exist;
   });
 });
